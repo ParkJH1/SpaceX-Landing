eab5d7ad3af9a4ddd1b851a8681e8cd383f296ec
박정현 <pjh9996@naver.com>
2021-11-12 12:30:21 +0900

    Lab02

--- Assets/Lab/Lab02/Scripts/CollisionTest.cs
+++ Assets/Lab/Lab02/Scripts/CollisionTest.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class CollisionTest : MonoBehaviour
 {
-    public Renderer renderer;
+    public Renderer rd;
+
+    private void Update()
+    {
+        rd.material.color = Color.cyan;
+    }
 
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.gameObject.name);
         Debug.Log(collision.relativeVelocity);
-        if (collision.relativeVelocity.y > 10f)
-        {
-            renderer.material.color = Color.red;
-        }
-        else
-        {
-            renderer.material.color = Color.green;
-        }
     }
 }
